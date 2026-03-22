9810bd48f9379b690ba09b3cb5845cafcff4d191
hsmtDP-T <ittuyo.hashi244@gmail.com>
2026-03-22 16:07:15 +0900

    profileセクション作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Footer from "./components/layout/Footer/Footer"
 import Header from "./components/layout/Header/Header"
+import Profile from './components/sections/Profile'
 import Top from "./components/sections/Top/Top"
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header></Header>
       <Top/>
+      <Profile/>
       <Footer/>
     </>
   )
