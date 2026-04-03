71659fb3979693a968d13e1fe04f25c99531f575
hsmtDP-T <ittuyo.hashi244@gmail.com>
2026-04-04 02:10:54 +0900

    formAPI結合

---
+++ frontend/src/features/form/postForm.ts
@@ -0,0 +1,17 @@
+import type { FormInput } from "./types";
+
+export const postForm = async (data: FormInput) => {
+    const response = await fetch("http://127.0.0.1:8787/form", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+    throw new Error("送信に失敗しました");
+    }
+
+    return response.json();
+};
