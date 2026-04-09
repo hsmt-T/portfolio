6e2bdece6854b1d825ea25999d6f05e046300853
hsmtDP-T <ittuyo.hashi244@gmail.com>
2026-04-09 22:20:27 +0900

    ポートフォリオセクションに新しいカードコンポーネントを追加し、スタイルを改善。スクロールダウンボタンの位置を調整し、ポートフォリオアイテムの型を定義。

--- frontend/src/components/sections/Portfolio/index.tsx
+++ frontend/src/components/sections/Portfolio/index.tsx
@@ -1,10 +1,31 @@
 import SectionTitle from "../../ui/SectionTitle";
 import "./Portfolio.css"
+import PortfolioItemCard from "../../ui/PortfolioItemCard";
+import ScrollDown from "../../ui/ScrollDown";
 
 const Portfolio = () => {
     return (
         <section className="portfolio" id="portfolio">
             <SectionTitle title="Portfolio"/>
+            <div className="portfolio-grid">
+                <PortfolioItemCard 
+                    title="Works" 
+                    description="今まで開発した作品をいくつか載せています"
+                />
+                <PortfolioItemCard 
+                    title="Skills" 
+                    description="使用経験がある技術を記載しています"
+                />
+                {/* <PortfolioItemCard 
+                    title="Intern" 
+                    description="参加させて頂いたインターンを記載しています。"
+                />
+                <PortfolioItemCard 
+                    title="Event" 
+                    description="参加させて頂いたイベントを記載しています。"
+                /> */}
+            </div>
+            <ScrollDown/>
         </section>
     )
 }
